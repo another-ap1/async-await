ea7e35ae4dca1648db4cab1b5b7f5ac5a0744e15
another-ap1 <grant.l.bollaert@hotmail.com>
2024-05-14 09:19:44 -0700

    adding to git hub

--- cards/cards.js
+++ cards/cards.js
@@ -1,71 +1,111 @@
-const newDeckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
 
-// function getaDeck(){
-//     axios
-//         .get(newDeckUrl)
-//         .then(res => {
-//             console.log(res);
-//             let deckId = res.data.deck_id;
-//             const drawCardUrl = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
-//             return axios.get(drawCardUrl)
-//         })
-//         .then(card => {
-//             console.log(card.data.cards[0].value,'of', card.data.cards[0].suit)
-//             console.log(card.data.cards[1].value,'of', card.data.cards[1].suit)
-//         })
-// }
+//WRITTEN WITH PROMISE
+// const newDeckUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
 
-$(function() {
-    let baseURL = 'https://deckofcardsapi.com/api/deck';
+// // function getaDeck(){
+// //     axios
+// //         .get(newDeckUrl)
+// //         .then(res => {
+// //             console.log(res);
+// //             let deckId = res.data.deck_id;
+// //             const drawCardUrl = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`
+// //             return axios.get(drawCardUrl)
+// //         })
+// //         .then(card => {
+// //             console.log(card.data.cards[0].value,'of', card.data.cards[0].suit)
+// //             console.log(card.data.cards[1].value,'of', card.data.cards[1].suit)
+// //         })
+// // }
+
+// $(function() {
+//     let baseURL = 'https://deckofcardsapi.com/api/deck';
   
-    // 1.
-    $.getJSON(`${baseURL}/new/draw/`).then(data => {
-      let { suit, value } = data.cards[0];
-      console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-    });
+//     // 1.
+//     $.getJSON(`${baseURL}/new/draw/`).then(data => {
+//       let { suit, value } = data.cards[0];
+//       console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+//     });
   
-    // 2.
-    let firstCard = null;
-    $.getJSON(`${baseURL}/new/draw/`)
-      .then(data => {
-        firstCard = data.cards[0];
-        let deckId = data.deck_id;
-        return $.getJSON(`${baseURL}/${deckId}/draw/`);
-      })
-      .then(data => {
-        let secondCard = data.cards[0];
-        [firstCard, secondCard].forEach(function(card) {
-          console.log(
-            `${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`
-          );
-        });
-      });
+//     // 2.
+//     let firstCard = null;
+//     $.getJSON(`${baseURL}/new/draw/`)
+//       .then(data => {
+//         firstCard = data.cards[0];
+//         let deckId = data.deck_id;
+//         return $.getJSON(`${baseURL}/${deckId}/draw/`);
+//       })
+//       .then(data => {
+//         let secondCard = data.cards[0];
+//         [firstCard, secondCard].forEach(function(card) {
+//           console.log(
+//             `${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`
+//           );
+//         });
+//       });
   
-    // 3.
-    let deckId = null;
-    let $btn = $('button');
-    let $cardArea = $('#card-area');
+//     // 3.
+//     let deckId = null;
+//     let $btn = $('button');
+//     let $cardArea = $('#card-area');
   
-    $.getJSON(`${baseURL}/new/shuffle/`).then(data => {
-      deckId = data.deck_id;
-      $btn.show();
-    });
+//     $.getJSON(`${baseURL}/new/shuffle/`).then(data => {
+//       deckId = data.deck_id;
+//       $btn.show();
+//     });
   
-    $btn.on('click', function() {
-      $.getJSON(`${baseURL}/${deckId}/draw/`).then(data => {
-        let cardSrc = data.cards[0].image;
-        let angle = Math.random() * 90 - 45;
-        let randomX = Math.random() * 40 - 20;
-        let randomY = Math.random() * 40 - 20;
-        $cardArea.append(
-          $('<img>', {
-            src: cardSrc,
-            css: {
-              transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
-            }
-          })
-        );
-        if (data.remaining === 0) $btn.remove();
-      });
-    });
-  });
+//     $btn.on('click', function() {
+//       $.getJSON(`${baseURL}/${deckId}/draw/`).then(data => {
+//         let cardSrc = data.cards[0].image;
+//         let angle = Math.random() * 90 - 45;
+//         let randomX = Math.random() * 40 - 20;
+//         let randomY = Math.random() * 40 - 20;
+//         $cardArea.append(
+//           $('<img>', {
+//             src: cardSrc,
+//             css: {
+//               transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+//             }
+//           })
+//         );
+//         if (data.remaining === 0) $btn.remove();
+//       });
+//     });
+//   });
+
+//WRITTEN WITH ASYNC/AWAIT
+const btn = document.querySelector('button');
+
+async function newDeck(){
+  res = await axios.get("https://deckofcardsapi.com/api/deck/new/")
+  deckId = res.data.deck_id;
+  shuffle(deckId);
+  return deckId
+}
+async function shuffle(id){
+  res = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/shuffle/`);
+  console.log(res)
+}
+async function drawCard(id){
+  let res = await axios.get(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=1`)
+  console.log(res.data);
+  appendCard(res.data.cards[0].image)
+  if(res.data.remaining === 0){
+    btn.remove()
+  }
+}
+
+let deckId = newDeck()
+btn.addEventListener('click', evt => {
+  evt.preventDefault()
+  
+  drawCard(deckId)
+})
+
+function appendCard(card){
+  const cardArea = document.querySelector('div');
+  let img = document.createElement('img');
+  img.src = card;
+
+  cardArea.append(img)
+
+}
--- favNumber/app.js
+++ favNumber/app.js
@@ -2,44 +2,90 @@ const favNum = document.querySelector("#favNum");
 const favTitle = document.querySelector("#favTitle");
 const numFactsList = document.querySelector("#numFactsList");
 const randFacts = document.querySelector("#randFacts");
-const li = document.createElement('li');
+const li1 = document.createElement('li');
 const li2 = document.createElement('li');
 const li3 = document.createElement('li');
 const li4 = document.createElement('li');
 
 
+//WRITTEN WITH PROMISE
+// addEventListener('submit', evt => {
+//     evt.preventDefault();
+//     userNum = favNum.value;
+//     const url = `http://numbersapi.com/${userNum}/trivia?json`;
+//     favTitle.style.display = "block";
+// //written with promise    
+//     axios
+//         .get(url)
+//         .then(res1 => {
+//             numFactsList.append(li)
+//             li.innerHTML = res1.data.text
+//             return axios.get(url)
+//         })
+//         .then(res2 => {
+//             numFactsList.append(li2)
+//             li2.innerHTML = res2.data.text
+//             return axios.get(url)
+//         })
+//         .then(res3 => {
+//             numFactsList.append(li3)
+//             li3.innerHTML = res3.data.text
+//             return axios.get(url)
+//         })
+//         .then(res4 => {
+//             numFactsList.append(li4)
+//             li4.innerHTML = res4.data.text
+//         })
+//         .catch(err => {
+//             console.log(`Ooops ${err}`)
+//         })
+// })
 
+//written with promise
+// const randUrl = "http://numbersapi.com/5..10/trivia?json"
+// axios
+//     .get(randUrl)
+//     .then(rand1 => {
+//         randFacts.append(li5)
+//         randFacts.append(li6)
+//         randFacts.append(li7)
+//         randFacts.append(li8)
+//         randFacts.append(li9)
+
+//         li5.innerHTML = rand1.data[5]
+//         li6.innerHTML = rand1.data[6]
+//         li7.innerHTML = rand1.data[7]
+//         li8.innerHTML = rand1.data[8]
+//         li9.innerHTML = rand1.data[9]
+//     })
+
+
+//WRITTEN WITH ASYNC/AWAIT
 addEventListener('submit', evt => {
     evt.preventDefault();
     userNum = favNum.value;
-    const url = `http://numbersapi.com/${userNum}/trivia?json`;
-    favTitle.style.display = "block";
-    
-    axios
-        .get(url)
-        .then(res1 => {
-            numFactsList.append(li)
-            li.innerHTML = res1.data.text
-            return axios.get(url)
-        })
-        .then(res2 => {
-            numFactsList.append(li2)
-            li2.innerHTML = res2.data.text
-            return axios.get(url)
-        })
-        .then(res3 => {
-            numFactsList.append(li3)
-            li3.innerHTML = res3.data.text
-            return axios.get(url)
-        })
-        .then(res4 => {
-            numFactsList.append(li4)
-            li4.innerHTML = res4.data.text
-        })
-        .catch(err => {
-            console.log(`Ooops ${err}`)
-        })
+    get4NumFacts(userNum);
 })
+
+async function get4NumFacts(num){
+    let baseUrl = `http://numbersapi.com/${userNum}/trivia?json`
+    let numFacts = await Promise.all([
+        axios.get(`${baseUrl}`),
+        axios.get(`${baseUrl}`),
+        axios.get(`${baseUrl}`),
+        axios.get(`${baseUrl}`)
+    ]);
+    
+    numFactsList.append(li1)
+    numFactsList.append(li2)
+    numFactsList.append(li3)
+    numFactsList.append(li4)
+
+    li1.innerHTML = numFacts[0].data.text;
+    li2.innerHTML = numFacts[1].data.text;
+    li3.innerHTML = numFacts[2].data.text;
+    li4.innerHTML = numFacts[3].data.text;
+}
 
 const li5 = document.createElement('li');
 const li6 = document.createElement('li');
@@ -47,19 +93,21 @@ const li7 = document.createElement('li');
 const li8 = document.createElement('li');
 const li9 = document.createElement('li');
 
-const randUrl = "http://numbersapi.com/5..10/trivia?json"
-axios
-    .get(randUrl)
-    .then(rand1 => {
-        randFacts.append(li5)
-        randFacts.append(li6)
-        randFacts.append(li7)
-        randFacts.append(li8)
-        randFacts.append(li9)
+//written with asyn/await
+async function randomNumFacts(){
+    const randUrl = "http://numbersapi.com/5..10/trivia?json";
+    let randomFacts = await axios.get(randUrl)
 
-        li5.innerHTML = rand1.data[5]
-        li6.innerHTML = rand1.data[6]
-        li7.innerHTML = rand1.data[7]
-        li8.innerHTML = rand1.data[8]
-        li9.innerHTML = rand1.data[9]
-    })
+    randFacts.append(li5)
+    randFacts.append(li6)
+    randFacts.append(li7)
+    randFacts.append(li8)
+    randFacts.append(li9)
+
+    li5.innerHTML = randomFacts.data[5]
+    li6.innerHTML = randomFacts.data[6]
+    li7.innerHTML = randomFacts.data[7]
+    li8.innerHTML = randomFacts.data[8]
+    li9.innerHTML = randomFacts.data[9]
+}
+randomNumFacts()
